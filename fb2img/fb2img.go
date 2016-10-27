5999d1d9f2c4042a04b7896f3ed6ffce93e251bc
Fernando Almeida <fernandoprsbr@gmail.com>
2016-10-27 21:24:51 -0200

    initial commit

---
+++ fb2img/fb2img.go
@@ -0,0 +1,48 @@
+package fb2img
+
+import (
+	"bytes"
+	"io/ioutil"
+	"fmt"
+	"html/template"
+	"os/exec"
+  "strings"
+  "math/rand"
+	"strconv"
+	"os"
+	"path/filepath"
+)
+
+var path, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+var templatepath = path + "\\" + "template.html";
+var htmltemplate, _ = template.ParseFiles(templatepath)
+
+func CreateImage(url string) (string, string) {	
+
+  templatebuff := bytes.NewBufferString("")
+	err := htmltemplate.Execute(templatebuff, url)
+	if err != nil {
+		panic(err)
+	}
+
+  tempstring := randomString();
+  htmlfile := path + "\\" + tempstring + ".html"
+	err = ioutil.WriteFile(htmlfile, templatebuff.Bytes(), 0666)
+	if err != nil {
+		panic(err)
+	}
+
+  imgfile := path + "\\" + tempstring + ".jpg";
+	err = exec.Command(path + "\\" + "wkhtmltoimage","--height", "500", "--width", "500", htmlfile, imgfile).Run()
+	if err != nil {
+		fmt.Printf("Error on trying to generate image: %s", err)
+	}
+
+	return imgfile, htmlfile
+}
+
+func randomString() string {
+    str := strconv.FormatFloat(rand.Float64(), 'f', 6, 64)
+		str = strings.Replace(str, ".", "", -1)
+		return str
+  }
---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"./fb2img"
+	"bytes"
+	"image"
+	"image/jpeg"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+// writeImage encodes an image 'img' in jpeg format and writes it into ResponseWriter.
+func writeImage(w http.ResponseWriter, img *image.Image) {
+
+	buffer := new(bytes.Buffer)
+	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+		// log.Println("unable to encode image.")
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		// log.Println("unable to write image.")
+	}
+}
+
+func serverHandler(w http.ResponseWriter, r *http.Request) {
+
+	url := r.URL.Query().Get("url")
+
+	imgpath, htmlpath := fb2img.CreateImage(url)
+	defer func() {
+		os.Remove(imgpath)
+		os.Remove(htmlpath)
+	}()
+
+	imgbuffer, _ := os.Open(imgpath)
+	defer imgbuffer.Close()
+
+	img_as_png, _, _ := image.Decode(imgbuffer)
+
+	writeImage(w, &img_as_png)
+}
+
+func main() {
+	http.HandleFunc("/fb2img", serverHandler)
+	http.ListenAndServe(":8080", nil)
+}
